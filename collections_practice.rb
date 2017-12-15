4f2853872d4cc5e68838ae8ac0fa18ed4c122b75
Fiona Lapham <fionadlapham@gmail.com>
2017-12-15 02:17:47 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,39 @@
 
+def sort_array_asc (array)
+  array.sort do |a, b|
+  a <=> b
+  end
+end
+
+def sort_array_desc (array)
+  array.sort do |a, b|
+    b<=>a
+  end
+end
+
+def sort_array_char_count (array)
+  array.sort do |a, b|
+     a.size <=> b.size
+  end
+end
+
+def swap_elements (array)
+  array[0], array[1], array [2] = array[0], array[2], array[1]
+end
+
+def reverse_array (array)
+  array.reverse
+end
+
+def kesha_maker (array)
+  array.map do |a|
+    a.slice!(2)
+    a.insert(2, "$")
+  end
+end
+
+def find_a (array)
+  array.find_all? |a|
+  a.start_with?("a")
+end
+end
